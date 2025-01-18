4c06a947de801bbc1c0a4212434defadce62a129
yhanaalvarez22 <botmaking402@gmail.com>
2025-01-19 00:30:04 +0800

    Update auto.js

--- auto.js
+++ auto.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const login = require('./fb-chat-api/index');
+const login = require('./heru-fca/index');
 const express = require('express');
 const app = express();
 const chalk = require('chalk');
